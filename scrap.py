d04695627426b49b360a6f7750088f7207f82651
Danang Agung Restu Aji <danangrestuaji@gmail.com>
2025-05-24 00:01:00 +0700

    scrap 1

--- scrap.py
+++ scrap.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from google_play_scraper import reviews
 
-def scrape_reviews(app_id: str, count: int = 1000, lang: str = 'id') -> pd.DataFrame:
+
+# Scraping Ulasan Aplikasi dari Google Play Store
+def scrape_reviews(app_id: str, count: int = 10000, lang: str = 'id') -> pd.DataFrame:
     result, _ = reviews(app_id, lang=lang, count=count)
     df = pd.DataFrame(result)
-    df = df[['content', 'score']]
+    # bagian berisi kolom yang ingin kita ambil
+    df = df[['userName', 'content', 'score', 'at']]
     return df
 
+# bagian ini untuk memberi label pada skor
 def label_sentiment(score: int) -> str:
     if score <= 2:
         return "negatif"
@@ -15,21 +19,22 @@ def label_sentiment(score: int) -> str:
     else:
         return "positif"
 
+# Fungsi untuk menyiapkan dataset
 def prepare_dataset(app_id: str, output_path: str = "dataset_labeled.csv") -> pd.DataFrame:
     df = scrape_reviews(app_id)
     df.drop_duplicates(subset=["content"], inplace=True)
     df.dropna(subset=["content"], inplace=True)
     df["label"] = df["score"].apply(label_sentiment)
     
-    # Menyimpan content, score, label, dan tanggal ke dalam CSV
-    df = df[["content", "score", "label", "at"]]
+    # Menyimpan userName, content, score, label, dan tanggal (at) ke dalam CSV
+    df = df[["userName", "content", "score", "label", "at"]]
     df.to_csv(output_path, index=False)
     
-    print(f"Dataset berhasil disimpan sebagai {output_path}")
+
+    print(f"Dataset berhasil disimpan: {output_path}")
     return df
 
-
-
+# Contoh penggunaan, saya mereview aplikasi Call of Duty Mobile misalnya
 if __name__ == "__main__":
     app_id = 'com.garena.game.codm'
     prepare_dataset(app_id)
